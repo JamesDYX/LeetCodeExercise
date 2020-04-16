2108c55363d21d7fccfc73a6368547d6f1f166cf
JamesDYX <jamesdyx@buaa.edu.cn>
2020-04-16 23:43:06 +0800

    clear all

--- 268_Missing_Number/solution_1.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    """
-    Gauss
-    """
-    def missingNumber(self, nums):
-        expected_sum = len(nums)*(len(nums)+1)//2
-        actual_sum = sum(nums)
-        return expected_sum - actual_sum
--- 268_Missing_Number/solution_2.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    """
-    Bit Manipulation
-    """
-    def missingNumber(self, nums):
-        missing = len(nums)
-        for i, num in enumerate(nums):
-            missing ^= i ^ num
-        return missing
