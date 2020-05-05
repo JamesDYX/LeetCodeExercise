39da3302a0b31f84d20b7ea6bc3f1ef022151436
JamesDYX <jamesdyx@buaa.edu.cn>
2020-05-05 12:36:58 +0800

    update

--- src/L297_Serialize_and_Deserialize_Binary_Tree/TreeNode.java
+++ src/L297_Serialize_and_Deserialize_Binary_Tree/TreeNode.java
@@ -4,8 +4,8 @@ public class TreeNode {
     public int val;
     public TreeNode left;
     public TreeNode right;
-    TreeNode(int x) { val = x; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
+    public TreeNode(int x) { val = x; }
+    public TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
         this.right = right;
